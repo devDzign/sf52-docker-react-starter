cef14a2152365d7ddcb8b5b5f1197fb35736be27
devDzign <chabour.mourad87@gmail.com>
2021-04-07 04:31:03 +0200

    feat(react) :
      add react
      add some components example
      add controller health

--- tests/Functional/HeathTest.php
+++ tests/Functional/HeathTest.php
@@ -12,6 +12,10 @@ class HeathTest extends WebTestCase
         $client = static::createClient();
         $crawler = $client->request('GET', '/');
 
+        $this->assertResponseIsSuccessful();
+
         $this->assertSelectorTextContains('h1', 'Hello World');
+        $this->assertSelectorTextContains('h2', 'Courte introduction à jQuery');
+
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,10 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .copyFiles({
+        from: './assets/images',
+        to: 'images/[path][name].[ext]'
+    })
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     // .enableStimulusBridge('./assets/controllers.json')
@@ -62,7 +66,7 @@ Encore
     //.enableTypeScriptLoader()
 
     // uncomment if you use React
-    //.enableReactPreset()
+    .enableReactPreset()
 
     // uncomment to get integrity="..." attributes on your script & link tags
     // requires WebpackEncoreBundle 1.4 or higher
